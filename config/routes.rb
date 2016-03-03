14da7e1ed9a76b27248f86b8b507e97bfcf7d19f
Sam Henderson <hendersonsam456@gmail.com>
2016-03-03 18:25:48 +1100

    getting donation new page to load and look like the other app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'donations/new'
 
+  resources :donations :only => [:new :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'donations#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
