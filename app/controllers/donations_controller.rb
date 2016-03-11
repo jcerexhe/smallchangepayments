e9cdd9985c0536c9448f4b37a211756362232931
Sam Henderson <hendersonsam456@gmail.com>
2016-03-11 15:56:54 +1100

    putting params in donation controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -20,13 +20,13 @@ class DonationsController < ApplicationController
 
     charge = Stripe::Charge.create(
       :source    => params[:stripeToken],
-      :amount      => 500,
+      :amount      => params[:amount],
       :description => params[:charity_name],
       :currency    => 'aud'
     )
 
     # Change these to params
-    payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 65,:user_id => 1,:charity_name => "Greenpeace"}}
+    payload = {:donation => {:amount => params[:amount], :charity_id => params[:charity], :submission_id => params[:submission_id], :user_id => params[:user_id],:charity_name => params[:charity_name]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 41,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
