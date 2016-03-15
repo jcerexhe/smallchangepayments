7a058cbe8c0b140de5bdaa24bf9e043d9c52c65a
Sam Henderson <hendersonsam456@gmail.com>
2016-03-15 14:31:09 +1100

    Added hidden field params

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -26,8 +26,10 @@ class DonationsController < ApplicationController
       :currency    => 'aud'
     )
 
+    @charity_id = params[:charity_id]
+
     # Change these to params
-    payload = {:donation => {:amount => params[:amount], :charity_id => params[:charity], :submission_id => params[:submission_id], :user_id => params[:user_id], :charity_name => params[:charity_name]}}
+    payload = {:donation => {:amount => @amount, :charity_id => @charity, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 98,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
