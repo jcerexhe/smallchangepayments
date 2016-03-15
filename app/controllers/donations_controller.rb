d785db3a993a7abf1f4eb771f3c9bc46eae2425f
Sam Henderson <hendersonsam456@gmail.com>
2016-03-15 15:18:48 +1100

    reverted last commit and added fields for testing

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -29,11 +29,17 @@ class DonationsController < ApplicationController
     )
 
     # Change these to params
-    payload = {:donation => {:amount => @amount.to_i, :charity_id => @charity.to_i, :submission_id => @submission_id.to_i, :user_id => @user_id.to_i, :charity_name => params[:charity_name]}}
+    payload = {:donation => {:amount => @amount, :charity_id => @charity, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 98,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
-      @conn.post "/donations/?amount=#{@amount}?user_id=#{@user_id}?charity_id=#{@charity_id}?submission_id=#{@submission_id}", payload
+      puts '*' * 80
+      puts payload
+      puts @user_id
+      puts @charity_id
+      puts @submission_id
+      @conn.post "/donations", payload
+      puts '*' * 80
     end
 
     rescue Stripe::CardError => e
