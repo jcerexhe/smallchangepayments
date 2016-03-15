7cbc9d061e9af74e51b695825cd3ecee30479bde
Sam Henderson <hendersonsam456@gmail.com>
2016-03-15 15:08:24 +1100

    Added to_i for params

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -17,19 +17,19 @@ class DonationsController < ApplicationController
 
     @user_id = params[:user_id]
     @charity_name = params[:charity_name]
+    @submission_id = params[:submission_id]
+    @charity_id = params[:charity_id]
     @amount = params[:amount] ? params[:amount] * 100 : 300
 
     charge = Stripe::Charge.create(
       :source    => params[:stripeToken],
-      :amount      => @amount,
+      :amount      => @amount.to_i,
       :description => params[:charity_name],
       :currency    => 'aud'
     )
 
-    @charity_id = params[:charity_id]
-
     # Change these to params
-    payload = {:donation => {:amount => @amount, :charity_id => @charity, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
+    payload = {:donation => {:amount => @amount.to_i, :charity_id => @charity.to_i, :submission_id => @submission_id.to_i, :user_id => @user_id.to_i, :charity_name => params[:charity_name]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 98,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
@@ -39,7 +39,7 @@ class DonationsController < ApplicationController
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to :new
-  end
+  end 
   
 end
 
