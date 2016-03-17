c79bbc10ddf80dd5777476ee035a9b4e524ce0ac
Jamie <jcerexhe@gmail.com>
2016-03-17 14:02:54 +1100

    Form works

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,7 +8,7 @@ class DonationsController < ApplicationController
     @charity_name = params[:charity_name]
     @submission_id = params[:submission_id]
     @charity_id = params[:charity_id]
-    @amount = params[:amount] ? params[:amount] * 100 : 300
+    @amount = params[:amount] ? (params[:amount]).to_i * 100 : 300
   end
 
   def create
@@ -22,37 +22,30 @@ class DonationsController < ApplicationController
     @charity_name = params[:charity_name]
     @submission_id = params[:submission_id]
     @charity_id = params[:charity_id]
-    @amount = params[:amount] ? params[:amount] * 100 : 300
+    @amount = params[:amount] ? (params[:amount]).to_i * 100 : 300
 
     charge = Stripe::Charge.create(
       :source    => params[:stripeToken],
-      :amount      => @amount.to_i,
+      :amount      => @amount,
       :description => params[:charity_name],
       :currency    => 'aud'
     )
 
     # Change these to params
-    payload = {:donation => {:amount => @amount, :charity_id => @charity, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
+    payload = {:donation => {:amount => @amount, :charity_id => @charity_id, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 98,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
-      puts '*' * 80
-      puts payload
-      puts @user_id
-      puts @charity_id
-      puts @submission_id
       @conn.post "/donations", payload
-      puts '*' * 80
     end
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to :new
-  end 
-  
+      redirect_to new_donation_path
+  end
+
 end
 
 
-      
 
-    
+
