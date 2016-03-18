c893a4ecc22c399774e3147c345278b131695f17
Jamie <jcerexhe@gmail.com>
2016-03-18 11:48:21 +1100

    Changed routes

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -23,6 +23,7 @@ class DonationsController < ApplicationController
     @submission_id = params[:submission_id]
     @charity_id = params[:charity_id]
     @amount = params[:amount] ? (params[:amount]).to_i * 100 : 300
+    @email = params[:email]
 
     charge = Stripe::Charge.create(
       :source    => params[:stripeToken],
@@ -32,7 +33,7 @@ class DonationsController < ApplicationController
     )
 
     # Change these to params
-    payload = {:donation => {:amount => @amount, :charity_id => @charity_id, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name]}}
+    payload = {:donation => {:amount => @amount, :charity_id => @charity_id, :submission_id => @submission_id, :user_id => @user_id, :charity_name => params[:charity_name], :email => params[:email]}}
     # payload = {:donation => {:amount => 1000, :charity_id => 1, :submission_id => 98,:user_id => 1,:charity_name => "Greenpeace"}}
 
     if charge['paid'] == true
@@ -42,6 +43,10 @@ class DonationsController < ApplicationController
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_donation_path
+
+    if response.code == 200
+      redirect_to "http://www.smallchangegiving.co/thanks?email=<%= @email %>&amount=<%= @amount %>&charity_id=<%= @charity_id %>&submission_id=<%= @submission_id %>"
+    end
   end
 
 end
